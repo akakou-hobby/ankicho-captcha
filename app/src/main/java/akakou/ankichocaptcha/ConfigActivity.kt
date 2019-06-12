25ecde9824903fb5d9daadb6f2e0e8c5dcd4a21e
akakou <s16003@tokyo.kosen-ac.j>
2019-06-12 23:59:18 +0900

    [create] Config Activity

---
+++ app/src/main/java/akakou/ankichocaptcha/ConfigActivity.kt
@@ -0,0 +1,12 @@
+package akakou.ankichocaptcha
+
+import android.app.Activity
+import android.os.Bundle
+
+class ConfigActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_config)
+    }
+}
